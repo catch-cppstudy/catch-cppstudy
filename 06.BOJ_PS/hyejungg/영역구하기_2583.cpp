3c046c1a7770329f04ccbc040ae8df26b2d3a48a
hyejungg <kimhyejung12@naver.com>
2021-11-29 14:16:05 +0900

    [6주차] 영역 구하기 (#2583)
    21/11/29
    ---
    처음에 dfs로 시도했는데 계속 틀렸습니다 떠서 bfs로도 했는데 그래도 계속 틀렸습니다 떠서 혹시나 하는 마음으로 set -> vector로 바꾸니까 통과 ..... 이유가 뭘까용 .....

---
+++ 06.BOJ_PS/hyejungg/영역구하기_2583.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+bool visit[101][101] = {
+    false,
+};
+int cnt, m, n, k;
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
+void dfs(int x, int y)
+{
+    visit[x][y] = true;
+    cnt++;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int nextX = x + dx[i];
+        int nextY = y + dy[i];
+
+        if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m)
+        {
+            if (!visit[nextX][nextY])
+                dfs(nextX, nextY);
+        }
+    }
+}
+
+void bfs(int x, int y)
+{
+    queue<pair<int, int>> q;
+    visit[x][y] = true;
+    q.push({x, y});
+    cnt = 1;
+
+    while (!q.empty())
+    {
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+
+            if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m)
+            {
+                if (!visit[nextX][nextY])
+                {
+                    q.push({nextX, nextY});
+                    visit[nextX][nextY] = true;
+                    cnt++;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> m >> n >> k;
+
+    for (int i = 0; i < k; i++)
+    {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        for (int r = x1; r < x2; r++)
+        {
+            for (int c = y1; c < y2; c++)
+            {
+                visit[r][c] = true;
+            }
+        }
+    }
+
+    // set<int> answer; //default : 오름차순 정렬 ................ 인데 set 쓰면 틀렸습니다 뜸 ...
+    vector<int> answer;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (!visit[i][j])
+            {
+                // cnt = 0;
+                // dfs(i, j);
+                bfs(i, j);
+                // answer.insert(cnt);
+                answer.push_back(cnt);
+            }
+        }
+    }
+
+    sort(answer.begin(), answer.end());
+    cout << answer.size() << "\n";
+    for (auto a : answer)
+        cout << a << " ";
+
+    return 0;
+}
